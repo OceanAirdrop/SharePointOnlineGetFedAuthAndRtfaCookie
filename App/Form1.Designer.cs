6528a525ed76f359991c82b8776013ff22e33cfd
Ocean Airdrop <ocean.airdrop@gmail.com>
2021-10-23 09:54:50 +0100

    Add files via upload

---
+++ App/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace WindowsFormsSharePointApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.buttonDownloadImage = new System.Windows.Forms.Button();
+            this.textBoxCookie = new System.Windows.Forms.TextBox();
+            this.textBoxFedAuth = new System.Windows.Forms.TextBox();
+            this.textBoxrtFa = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxImageName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(15, 30);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(342, 223);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.Url = new System.Uri("", System.UriKind.Relative);
+            this.webBrowser1.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser1_Navigated);
+            // 
+            // buttonDownloadImage
+            // 
+            this.buttonDownloadImage.Location = new System.Drawing.Point(662, 396);
+            this.buttonDownloadImage.Name = "buttonDownloadImage";
+            this.buttonDownloadImage.Size = new System.Drawing.Size(126, 37);
+            this.buttonDownloadImage.TabIndex = 2;
+            this.buttonDownloadImage.Text = "Download Programmatically";
+            this.buttonDownloadImage.UseVisualStyleBackColor = true;
+            this.buttonDownloadImage.Click += new System.EventHandler(this.buttonDownloadImage_Click);
+            // 
+            // textBoxCookie
+            // 
+            this.textBoxCookie.Location = new System.Drawing.Point(383, 30);
+            this.textBoxCookie.Multiline = true;
+            this.textBoxCookie.Name = "textBoxCookie";
+            this.textBoxCookie.Size = new System.Drawing.Size(405, 140);
+            this.textBoxCookie.TabIndex = 3;
+            // 
+            // textBoxFedAuth
+            // 
+            this.textBoxFedAuth.Location = new System.Drawing.Point(383, 196);
+            this.textBoxFedAuth.Multiline = true;
+            this.textBoxFedAuth.Name = "textBoxFedAuth";
+            this.textBoxFedAuth.ReadOnly = true;
+            this.textBoxFedAuth.Size = new System.Drawing.Size(405, 57);
+            this.textBoxFedAuth.TabIndex = 4;
+            // 
+            // textBoxrtFa
+            // 
+            this.textBoxrtFa.Location = new System.Drawing.Point(383, 285);
+            this.textBoxrtFa.Multiline = true;
+            this.textBoxrtFa.Name = "textBoxrtFa";
+            this.textBoxrtFa.ReadOnly = true;
+            this.textBoxrtFa.Size = new System.Drawing.Size(405, 57);
+            this.textBoxrtFa.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(380, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "CookieData From Web Browser";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(380, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "FedAuth Cookie";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(380, 269);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Rtfa Cookie";
+            // 
+            // textBoxImageName
+            // 
+            this.textBoxImageName.Location = new System.Drawing.Point(383, 413);
+            this.textBoxImageName.Name = "textBoxImageName";
+            this.textBoxImageName.Size = new System.Drawing.Size(232, 20);
+            this.textBoxImageName.TabIndex = 9;
+            this.textBoxImageName.Text = "P08884-636977513961360015.JPG";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(380, 396);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Picture To Download";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(268, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Initial Web Browser (To Snatch Cookies From)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 268);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(217, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Programmaticallly Downloaded Image";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 285);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(342, 190);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 497);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxImageName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxrtFa);
+            this.Controls.Add(this.textBoxFedAuth);
+            this.Controls.Add(this.textBoxCookie);
+            this.Controls.Add(this.buttonDownloadImage);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button buttonDownloadImage;
+        private System.Windows.Forms.TextBox textBoxCookie;
+        private System.Windows.Forms.TextBox textBoxFedAuth;
+        private System.Windows.Forms.TextBox textBoxrtFa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxImageName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ App/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsSharePointApp2
+{
+    // https://docs.microsoft.com/en-us/previous-versions/office/developer/sharepoint-2010/hh147177(v=office.14)?redirectedfrom=MSDN
+    // https://stackoverflow.com/questions/15049877/getting-webbrowser-cookies-to-log-in
+    // https://stackoverflow.com/questions/3382498/is-it-possible-to-transfer-authentication-from-webbrowser-to-webrequest
+    // https://stackoverflow.com/questions/3062925/c-sharp-get-httponly-cookie
+    // https://stackoverflow.com/questions/25388696/federated-authentication-in-sharepoint-2013-getting-rtfa-and-fedauth-cookies
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Initially navigate to a page that the user will have access to, to grab the FedAuth & rtFa cookies.
+            this.webBrowser1.Navigate(@"https://xx.sharepoint.com/xx/xx/xx/Forms/AllItems.aspx");
+        }
+
+        private void webBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            try
+            {
+                if (webBrowser1.Url.AbsoluteUri == "about:blank")
+                    return;
+
+                var cookieData = GetWebBrowserCookie.GetCookieInternal(webBrowser1.Url, false);
+
+                if (string.IsNullOrEmpty(cookieData) == false)
+                {
+                    textBoxCookie.Text = cookieData;
+
+                    var dict = ParseCookieData(cookieData);
+                    textBoxFedAuth.Text = dict["FedAuth"];
+                    textBoxrtFa.Text = dict["rtFa"];
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private IDictionary<string, string> ParseCookieData(string cookieData)
+        {
+            var cookieDictionary = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+            try
+            {
+                if (string.IsNullOrEmpty(cookieData))
+                    return cookieDictionary;
+
+                var values = cookieData.TrimEnd(';').Split(';');
+                foreach (var parts in values.Select(c => c.Split(new[] { '=' }, 2)))
+                {
+                    var cookieName = parts[0].Trim();
+                    string cookieValue;
+
+                    if (parts.Length == 1)
+                        cookieValue = string.Empty;
+                    else
+                        cookieValue = parts[1];
+
+                    cookieDictionary[cookieName] = cookieValue;
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            return cookieDictionary;
+        }
+
+        private void buttonDownloadImage_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var url = $"https://xx.sharepoint.com/sites/xx/Images/{textBoxImageName.Text}";
+
+                var handler = new HttpClientHandler();
+                handler.CookieContainer = new System.Net.CookieContainer();
+
+                var cc = new CookieCollection();
+                cc.Add(new Cookie("FedAuth", textBoxFedAuth.Text));
+                cc.Add(new Cookie("rtFa", textBoxrtFa.Text));
+
+                handler.CookieContainer.Add(new Uri(url), cc);
+
+                HttpClient httpClient = new HttpClient(handler);
+                var resp = httpClient.GetAsync(url).Result;
+                var byteData = resp.Content.ReadAsByteArrayAsync().Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    pictureBox1.Image = byteArrayToImage(byteData);
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        public Image byteArrayToImage(byte[] bytesArr)
+        {
+            using (MemoryStream memstr = new MemoryStream(bytesArr))
+            {
+                Image img = Image.FromStream(memstr);
+                return img;
+            }
+        }
+    }
+}
